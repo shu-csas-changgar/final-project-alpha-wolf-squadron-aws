124925c093e84b3c0521c2d223e87fc078e2730a
weeks4397 <alexander.weeks@studnet.shu.edu>
2019-03-20 14:27:01 -0400

    added to the web page and to the eer diagram

--- DataBaseWebsite/src/app/app.module.ts
+++ DataBaseWebsite/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
