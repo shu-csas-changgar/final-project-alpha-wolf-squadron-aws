4652a0925f0af65758f8b1d3f4e5d7784740c4e6
Jherry <39573971+jcalderon927@users.noreply.github.com>
2019-03-25 11:52:15 -0400

    home page now first page user sees. Button at top leads to log in

--- DataBaseWebsite/src/app/app-routing.module.ts
+++ DataBaseWebsite/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from "./home-page/home-page.component"
 import { LoginPageComponent } from './login-page/login-page.component';
 const routes: Routes = [
-  {path:'', component: HomePageComponent},
+  {path: '', component: HomePageComponent},
   {path: 'home-page', component: HomePageComponent},
   {path: 'login-page', component: LoginPageComponent}
 ];
