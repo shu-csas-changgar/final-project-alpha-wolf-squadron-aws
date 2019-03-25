5cb8c8c3ac093c38957f4b4db1c5403df6b7b98e
Jherry <39573971+jcalderon927@users.noreply.github.com>
2019-03-25 11:33:15 -0400

    added routing to the home page. login still doesnt work based on  credentials. but you can log out

--- DataBaseWebsite/src/app/app-routing.module.ts
+++ DataBaseWebsite/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { HomePageComponent } from "./home-page/home-page.component"
+import { LoginPageComponent } from './login-page/login-page.component';
+const routes: Routes = [
+  {path:'', component: HomePageComponent},
+  {path: 'home-page', component: HomePageComponent},
+  {path: 'login-page', component: LoginPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
