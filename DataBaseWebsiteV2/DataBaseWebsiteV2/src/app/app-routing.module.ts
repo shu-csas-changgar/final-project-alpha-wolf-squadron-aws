210e0e040c54131c2bfcf3b92b2b3c24395cd0e8
Jherry <39573971+jcalderon927@users.noreply.github.com>
2019-03-29 13:34:39 -0400

    added website v2

---
+++ DataBaseWebsiteV2/DataBaseWebsiteV2/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {path:'', redirectTo: '/login', pathMatch: 'full'},
+  {path:'home', component: HomePageComponent},
+  {path:'login', component: LoginPageComponent},
+  {path:'page1', component: Page1Component},
+  {path:'page2', component: Page2Component},
+  {path:'**', component: PageNotFoundComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ DataBaseWebsiteV2/DataBaseWebsiteV2/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RootNavigationComponent } from './root-navigation/root-navigation.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    Page1Component,
+    Page2Component,
+    PageNotFoundComponent,
+    RootNavigationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
