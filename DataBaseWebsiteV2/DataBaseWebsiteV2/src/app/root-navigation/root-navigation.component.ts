7df2b91850e5e8316103bb1781b69a5566aa6c8c
weeks4397 <alexander.weeks@studnet.shu.edu>
2019-03-29 21:26:12 -0400

    updated wed page added css

--- DataBaseWebsiteV2/DataBaseWebsiteV2/src/app/root-navigation/root-navigation.component.ts
+++ DataBaseWebsiteV2/DataBaseWebsiteV2/src/app/root-navigation/root-navigation.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root-navigation',
   templateUrl: './root-navigation.component.html',
-  styleUrls: ['../app.component.css']
+  styleUrls: ['./root-navigation.component.css']
 })
 export class RootNavigationComponent implements OnInit {
 
