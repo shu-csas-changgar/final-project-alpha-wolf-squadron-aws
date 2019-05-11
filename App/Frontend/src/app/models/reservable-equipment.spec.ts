92834c776cb5615eb08da9e8f4cb4fc4cd97b597
garrett-denton <35875712+garrett-denton@users.noreply.github.com>
2019-05-11 18:21:29 -0400

    dumb res

--- App/Frontend/src/app/models/reservable-equipment.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ReservableEquipment } from './reservable-equipment';
-
-describe('ReservableEquipment', () => {
-  it('should create an instance', () => {
-    expect(new ReservableEquipment(1,0,1,'05/02/2019','05/03/2019')).toBeTruthy();
-  });
-});
--- App/Frontend/src/app/models/reservation-equipment.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ReservationEquipment } from './reservation-equipment';
-
-describe('ReservationEquipment', () => {
-  it('should create an instance', () => {
-    expect(new ReservationEquipment(1,1,'05/01/2019','05/02/2019')).toBeTruthy();
-  });
-});
